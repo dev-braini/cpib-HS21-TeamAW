893a918de38f247a005567928c4471a617abcffc
Markus Winter <markus.winter@students.fhnw.ch>
2021-09-28 17:00:48 +0200

    Add project structure

---
+++ src/main/java/ch/fhnw/cpib/Compiler.java
@@ -0,0 +1,7 @@
+package ch.fhnw.cpib;
+
+public class Compiler {
+    public static void main(String[] args) {
+        System.out.println("Hello Manuel, Hi Markus!");
+    }
+}
---
+++ src/test/java/ch/fhnw/cpib/CompilerTest.java
@@ -0,0 +1,15 @@
+package ch.fhnw.cpib;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CompilerTest {
+    @Test
+    @DisplayName("AssertEquals Test")
+    void testMultiply() {
+        assertEquals(1, 1, "assertEquals works :)");
+    }
+
+}
