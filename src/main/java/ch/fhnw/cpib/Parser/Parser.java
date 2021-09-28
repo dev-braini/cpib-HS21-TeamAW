cff7db2b3f16e692560b8f9f6ed0cf7d1c4e2dee
Attiger Manuel (s) <manuel.attiger@students.fhnw.ch>
2021-09-28 18:28:23 +0200

    Testcommit MA

---
+++ src/main/java/ch/fhnw/cpib/Parser/Parser.java
@@ -0,0 +1,15 @@
+package ch.fhnw.cpib.Parser;
+
+/**
+ * I'm a parser
+ */
+
+public class Parser {
+    public Parser(){
+
+    }
+
+    public void parse() {
+        System.out.println("parse...");
+    }
+}
