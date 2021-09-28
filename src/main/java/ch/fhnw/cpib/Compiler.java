be6b2e17391abdc974e5badf49f782a09ba908a0
Markus Winter <markus.winter@students.fhnw.ch>
2021-09-28 17:38:19 +0200

    Base structure Scanner

--- src/main/java/ch/fhnw/cpib/Compiler.java
+++ src/main/java/ch/fhnw/cpib/Compiler.java
@@ -1,7 +1,16 @@
 package ch.fhnw.cpib;
 
+import ch.fhnw.cpib.Scanner.Scanner;
+
 public class Compiler {
+    public Compiler() {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Manuel, Hi Markus!");
+
+        Scanner scanner = new Scanner();
+        scanner.scan();
     }
 }
--- src/main/java/ch/fhnw/cpib/Scanner/Scanner.java
+++ src/main/java/ch/fhnw/cpib/Scanner/Scanner.java
@@ -4,4 +4,11 @@ package ch.fhnw.cpib.Scanner;
  * I'm a scanner
  */
 public class Scanner {
+    public Scanner() {
+
+    }
+
+    public void scan() {
+        System.out.println("scan...");
+    }
 }
