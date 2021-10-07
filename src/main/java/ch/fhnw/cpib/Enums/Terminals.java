2fbd223b70b67835ad153698d7231c5a3c5079d8
Attiger Manuel (s) <manuel.attiger@students.fhnw.ch>
2021-10-07 19:59:29 +0200

    Added Terminals

--- src/main/java/ch/fhnw/cpib/Enums/Terminals.java
+++ src/main/java/ch/fhnw/cpib/Enums/Terminals.java
@@ -1,12 +1,48 @@
 package ch.fhnw.cpib.Enums;
 
 public enum Terminals {
-    //ToDo: Fill in more Terminals
-    IDENT,
-    BOOLOPR,
+
     ADDOPR,
-    LITERAL,
     MULTOPR,
-    SENTINEL,
-    RELOPR;
+    RELOPR,
+    BOOLOPR,
+
+    CHANGEMODE,
+    MECHMODE,
+    FLOWMODE,
+
+    LPAREN,
+    RPAREN,
+    COMMA,
+    SEMICOLON,
+    COLON,
+    BECOMES,
+    IF,
+    ELSE,
+    ENDIF,
+    FUN,
+    GLOBAL,
+    LOCAL,
+    INIT,
+    PROGRAM,
+    TYPE,
+    CALL,
+    DEBUGIN,
+    DEBUGOUT,
+    PROC,
+    RETURNS,
+    SKIP,
+    THEN,
+    WHILE,
+    DO,
+    ENDWHILE,
+    ENDPROGRAM,
+    ENDFUN,
+    ENDPROC,
+
+    LITERAL,
+    IDENT,
+
+    SENTINEL;
+
 }
