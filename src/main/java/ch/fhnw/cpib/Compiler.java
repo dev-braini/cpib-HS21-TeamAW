7a6ef0dfeffdfa11531b46f0e0096c0bd9140c50
Markus Winter <markus.winter@students.fhnw.ch>
2021-10-09 10:47:00 +0200

    Add example iml-programm code (Add17.iml)

--- src/main/java/ch/fhnw/cpib/Compiler.java
+++ src/main/java/ch/fhnw/cpib/Compiler.java
@@ -2,16 +2,24 @@ package ch.fhnw.cpib;
 
 import ch.fhnw.cpib.Errors.LexicalError;
 import ch.fhnw.cpib.Scanner.Scanner;
+import ch.fhnw.cpib.Token.ITokenList;
+
+import java.io.*;
 
 public class Compiler {
-    public Compiler() {
+    private static String IML_TEST_CODE = "program Add17()\nglobal\n  var x:int32\ndo\n  debugin x init;\n  x := x + 17;\n  debugout x\nendprogram";
 
-    }
-
-    public static void main(String[] args) throws LexicalError {
-        System.out.println("Hello Manuel, Hi Markus!");
-
+    public static void main(String[] args) throws LexicalError, IOException {
+        ITokenList tokenList;
         Scanner scanner = new Scanner();
-        scanner.scan("");
+        tokenList = scanner.scan(IML_TEST_CODE);
+
+        System.out.println("INPUT (IML):");
+        System.out.println("---------------------");
+        System.out.println(IML_TEST_CODE + "\n");
+
+        System.out.println("OUTPUT (ITokenList):");
+        System.out.println("---------------------");
+        System.out.println(tokenList.toString());
     }
 }
