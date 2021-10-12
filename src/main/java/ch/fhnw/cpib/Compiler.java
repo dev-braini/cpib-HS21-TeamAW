66f653d26c93fbed35364139e4f2b2749d7f6f36
Markus Winter <markus.winter@students.fhnw.ch>
2021-10-12 14:39:01 +0200

    Make IML_TEST_CODE final

--- src/main/java/ch/fhnw/cpib/Compiler.java
+++ src/main/java/ch/fhnw/cpib/Compiler.java
@@ -4,12 +4,10 @@ import ch.fhnw.cpib.Errors.LexicalError;
 import ch.fhnw.cpib.Scanner.Scanner;
 import ch.fhnw.cpib.Token.ITokenList;
 
-import java.io.*;
-
 public class Compiler {
-    private static String IML_TEST_CODE = "program Add17()\nglobal\n  var x:int32\ndo\n  debugin x init;\n  x := x + 17;\n  debugout x\nendprogram";
+    private static final String IML_TEST_CODE = "program Add17()\nglobal\n  var x:int32\ndo\n  debugin x init;\n  x := x + 17;\n  debugout x\nendprogram";
 
-    public static void main(String[] args) throws LexicalError, IOException {
+    public static void main(String[] args) throws LexicalError {
         ITokenList tokenList;
         Scanner scanner = new Scanner();
         tokenList = scanner.scan(IML_TEST_CODE);
