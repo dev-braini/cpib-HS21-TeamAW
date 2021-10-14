4e9a6d6a309de3231600164dcc5a4f0ee1d94b91
Markus Winter <markus.winter@students.fhnw.ch>
2021-10-14 22:21:26 +0200

    Add parser (first draft)

---
+++ src/main/java/ch/fhnw/cpib/Parser/IParser.java
@@ -0,0 +1,5 @@
+package ch.fhnw.cpib.Parser;
+
+public interface IParser {
+    // IConcSyn.IProgram parse() throws GrammerError;
+}
--- src/main/java/ch/fhnw/cpib/Parser/Parser.java
+++ src/main/java/ch/fhnw/cpib/Parser/Parser.java
@@ -1,15 +1,43 @@
 package ch.fhnw.cpib.Parser;
 
+import ch.fhnw.cpib.Enums.Terminals;
+import ch.fhnw.cpib.Errors.GrammarError;
+import ch.fhnw.cpib.Token.IToken;
+import ch.fhnw.cpib.Token.ITokenList;
+
 /**
  * I'm a parser
  */
 
-public class Parser {
-    public Parser(){
+public class Parser implements IParser {
+    private final ITokenList tokenList;
+    private IToken token;
+    private Terminals terminal;
 
+    public Parser(ITokenList tokenList){
+        this.tokenList = tokenList;
+        this.tokenList.reset();
+        token = tokenList.nextToken();
+        terminal = token.getTerminal();
+        // concSyn = new ConcSyn();
     }
 
-    public void parse() {
-        System.out.println("parse...");
+    private IToken consume(Terminals expectedTerminal) throws GrammarError {
+        if (terminal == expectedTerminal) {
+            IToken consumedToken = token;
+            if (terminal != Terminals.SENTINEL) {
+                token = tokenList.nextToken();
+                terminal = token.getTerminal();
+            }
+            return consumedToken;
+        } else {
+            throw new GrammarError("Terminal expected: " + expectedTerminal + ", Terminal found: " + terminal);
+        }
+    }
+
+    public void parse() throws GrammarError {
+        // ConcSyn.IProgram program = program();
+        consume(Terminals.SENTINEL);
+        //return program
     }
 }
