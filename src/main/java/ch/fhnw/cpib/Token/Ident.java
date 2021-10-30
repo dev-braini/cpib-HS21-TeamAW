92044946ded045cb67584450b27e839e04bff001
Markus Winter <markus.winter@students.fhnw.ch>
2021-10-30 16:21:40 +0200

    Space typo

--- src/main/java/ch/fhnw/cpib/Token/Ident.java
+++ src/main/java/ch/fhnw/cpib/Token/Ident.java
@@ -2,7 +2,7 @@ package ch.fhnw.cpib.Token;
 
 import ch.fhnw.cpib.Enums.Terminals;
 
-public class Ident extends Token{
+public class Ident extends Token {
     private final String value;
 
     public Ident(String value){
