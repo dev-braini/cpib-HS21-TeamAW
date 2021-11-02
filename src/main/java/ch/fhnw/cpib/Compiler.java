d3c2b3cc8f868c0e303ab52ed98b4b6909fe038a
Markus Winter <markus.winter@students.fhnw.ch>
2021-11-02 17:16:54 +0100

    Add attention-comment (blank line)

--- src/main/java/ch/fhnw/cpib/Compiler.java
+++ src/main/java/ch/fhnw/cpib/Compiler.java
@@ -7,6 +7,7 @@ import ch.fhnw.cpib.Scanner.Scanner;
 import ch.fhnw.cpib.Token.ITokenList;
 
 public class Compiler {
+    /* ATTENTION: IML needs a blank line at the end */
     private static final String IML_TEST_CODE = "//I'm a super program\nprogram Add17\nglobal\n var x:int64\ndo\n  debugin x init;\n  x := x + 17;\n  debugout x\n endprogram \n";
 
     public static void main(String[] args) throws LexicalError, GrammarError {
