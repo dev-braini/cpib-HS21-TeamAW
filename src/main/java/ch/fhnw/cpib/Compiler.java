0412c5fa86c6ec3b16caca4903f26b17729d5e60
Markus Winter <markus.winter@students.fhnw.ch>
2021-11-02 17:48:36 +0100

    Read iml code from file

--- src/main/java/ch/fhnw/cpib/Compiler.java
+++ src/main/java/ch/fhnw/cpib/Compiler.java
@@ -6,18 +6,25 @@ import ch.fhnw.cpib.Parser.Parser;
 import ch.fhnw.cpib.Scanner.Scanner;
 import ch.fhnw.cpib.Token.ITokenList;
 
+import java.io.*;
+
 public class Compiler {
     /* ATTENTION: IML needs a blank line at the end */
-    private static final String IML_TEST_CODE = "//I'm a super program\nprogram Add17\nglobal\n var x:int64\ndo\n  debugin x init;\n  x := x + 17;\n  debugout x\n endprogram \n";
+    private static final String IML_TEST_CODE_FILE = "add17.iml";
 
-    public static void main(String[] args) throws LexicalError, GrammarError {
+    public static void main(String[] args) throws LexicalError, GrammarError, FileNotFoundException {
+
+        // read iml code sample
+        StringBuilder imlCode = readIMLCode();
+
+
         ITokenList tokenList;
         Scanner scanner = new Scanner();
-        tokenList = scanner.scan(IML_TEST_CODE);
+        tokenList = scanner.scan(imlCode);
 
         System.out.println("INPUT (IML):");
         System.out.println("---------------------");
-        System.out.println(IML_TEST_CODE + "\n");
+        System.out.println(imlCode + "\n");
 
         System.out.println("OUTPUT (ITokenList):");
         System.out.println("---------------------");
@@ -27,5 +34,32 @@ public class Compiler {
 
         Parser parser = new Parser(tokenList);
         parser.parse();
+    }
+
+    private static StringBuilder readIMLCode() throws FileNotFoundException {
+        String filePath = "src/main/resources/IML_samples/" + IML_TEST_CODE_FILE;
+        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(filePath));
+        BufferedReader bufferedReader = null;
+        String line;
+        StringBuilder s = new StringBuilder();
+
+        try {
+            bufferedReader = new BufferedReader(inputStreamReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                s.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading iml code sample");
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+
+        return s;
     }
 }
