949d72fa5d33ddd10ab8732af67a63716e27af59
Markus Winter <markus.winter@students.fhnw.ch>
2021-12-13 01:52:10 +0100

    fix MonadicOpr

--- src/main/java/ch/fhnw/cpib/Token/MonadicOpr.java
+++ src/main/java/ch/fhnw/cpib/Token/MonadicOpr.java
@@ -3,13 +3,13 @@ package ch.fhnw.cpib.Token;
 import ch.fhnw.cpib.Enums.Terminals;
 
 public class MonadicOpr extends Token {
-    private Token operator;
+    private IToken operator;
 
-    public MonadicOpr(Terminals terminal) {
-        super(terminal);
+    public MonadicOpr(IToken operator) {
+        super(operator.getTerminal());
     }
 
-    public Token getOperator() {
-        return this;
+    public IToken getOperator() {
+        return operator;
     }
 }
