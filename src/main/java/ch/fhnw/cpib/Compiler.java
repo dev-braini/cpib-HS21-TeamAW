fde413a604ad46b6cc324d165b7b7e41a06f40bd
Markus Winter <markus.winter@students.fhnw.ch>
2021-12-14 23:44:51 +0100

    Add error handling

--- src/main/java/ch/fhnw/cpib/Compiler.java
+++ src/main/java/ch/fhnw/cpib/Compiler.java
@@ -1,7 +1,6 @@
 package ch.fhnw.cpib;
 
-import ch.fhnw.cpib.Errors.GrammarError;
-import ch.fhnw.cpib.Errors.LexicalError;
+import ch.fhnw.cpib.Errors.*;
 import ch.fhnw.cpib.Helper.ConsoleWriter;
 import ch.fhnw.cpib.Helper.IMLReader;
 import ch.fhnw.cpib.Parser.AbstractSyntaxTree.AbsSyn;
@@ -12,7 +11,7 @@ import ch.fhnw.cpib.Token.ITokenList;
 import java.io.*;
 
 public class Compiler {
-    public static void main(String[] args) throws GrammarError, FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, NameAlreadyDeclaredError, NameAlreadyGloballyDeclaredError, NameNotDeclaredError, CaseAlreadyDeclaredError, InvalidParamCountError, LRValError, DefaultCaseBoolOverkillError, AlreadyInitializedError {
         // read iml code sample
         Scanner scanner = new Scanner();
         ConsoleWriter cw = new ConsoleWriter();
@@ -37,7 +36,27 @@ public class Compiler {
         cw.write("TokenList (vertical)");
         tokenList.print();
         cw.write("OUTPUT (Parser)");
-        absSyn = parser.parse();
+        try {
+            absSyn = parser.parse();
+        }
+        catch (GrammarError e) { System.out.println("Parser error"); e.printStackTrace(); }
+        /* catch (NameAlreadyDeclaredError e) { System.out.println("Namespace check error"); e.printStackTrace(); }
+        catch (NameNotDeclaredError e) { System.out.println("Namespace check error"); e.printStackTrace(); }
+        catch (NameAlreadyGloballyDeclaredError e) { System.out.println("Namespace check error"); e.printStackTrace(); }
+        catch (LRValError e) { System.out.println("LRVal check error"); e.printStackTrace(); }
+        catch (InvalidParamCountError e) { System.out.println("Param check error"); e.printStackTrace(); }
+        catch (AlreadyInitializedError e) { System.out.println("Init check error"); e.printStackTrace(); }
+        catch (DefaultCaseBoolOverkillError e) { System.out.println("Switch case error"); e.printStackTrace(); }
+        catch (CaseAlreadyDeclaredError e) { System.out.println("Switch case error"); e.printStackTrace(); }
+        catch (TypeCheckError e) { System.out.println("Type check error"); e.printStackTrace(); }
+        catch (NotInitializedError e) { System.out.println("Init check error"); e.printStackTrace(); }
+        catch (GlobalInitializationProhibitedError e) { System.out.println("Init check error"); e.printStackTrace(); }
+        catch (CannotAssignToConstError e) { System.out.println("Const error"); e.printStackTrace(); }
+        */
+
+        assert absSyn != null;
+        cw.write("INPUT (IML)");
+        // absSyn.doScopeChecking();
     }
 
 
